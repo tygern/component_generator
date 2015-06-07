f64e8c77058b2db982adfb327619af4fb653a103
Tyson Gern <tygern@gmail.com>
2015-06-07 16:54:38 -0600

    Move generator files

--- lib/component_generator/version.rb
+++ lib/component_generator/version.rb
@@ -1,3 +1,3 @@
 module ComponentGenerator
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
---
+++ lib/generators/component_generator/component_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators'
+
+module ComponentGenerator
+  class ComponentGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+
+    def copy_component_directory
+      self.destination_root = "components"
+
+      directory "engine_template", file_name
+      chmod "#{file_name}/test.sh", 0755
+    end
+  end
+end
